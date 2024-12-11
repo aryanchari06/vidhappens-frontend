0ee574693c67f8eb3a1d0d5e5c6711eeac3c6e50
aryanchari06 <aryaney0704@gmail.com>
2024-12-11 22:14:23 +0530

    bug fix -1

--- src/Components/index.js
+++ src/Components/index.js
@@ -5,7 +5,7 @@ import Sidebar from "./Sidebar/Sidebar";
 import Login from "./Login/Login";
 import Signup from "./Signup/Signup";
 import UserChannel from "./UserChannel/UserChannel";
-import UserVideos from "./UserVideos/userVideos";
+import UserVideos from "./UserVideos/UserVideos";
 import LikedVideos from "./LikedVideos/LikedVideos";
 import UserPlaylists from "./UserPlaylists/UserPlaylists";
 import WatchHistory from "./WatchHistory/WatchHistory";
